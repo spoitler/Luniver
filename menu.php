965b8e526381e4fcaa06c36c8400151f2760f417
spoitler <romain.bonnes@gmail.com>
2019-04-17 17:51:10 +0200

    bug 0 sur mobile*

--- menu.php
+++ menu.php
@@ -45,7 +45,7 @@ session_start();
 <div id="shopping-cart">
     <a href="panier.php">
         <img id="img_shopping_cart" src="img/shopping-cart-vide.png">
-        <div id="notification-icon"><span><?= array_sum($_SESSION['panier']); ?></span></div>
+        <div id="notification-icon"><span><?php if(array_sum($_SESSION['panier']) == 0){ echo "0";}else{ echo array_sum($_SESSION['panier']);} ?></span></div>
     </a>
 </div>
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
