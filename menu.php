ad53ac84d9d5160a0a583301a72ffb9610a41450
Spoitler <romain.bonnes@gmail.com>
2019-04-25 15:39:39 +0200

    test menu petit ecran

--- menu.php
+++ menu.php
@@ -36,8 +36,10 @@ if (!isset($_SESSION['panier'])){
                                   <li><a class="accordeon_cont" href="recupdonneeperso.php">Données personnelles</a></li>
                                 </ul>
                             </li>
-                            <li><a href="deconnexion.php">DECONNEXION</a></li>';
-                    }?>
+                            <li><a href="deconnexion.php">DECONNEXION</a></li>
+<li><a href="deconnexion.php">DECONNEXION</a></li>
+<li><a href="deconnexion.php">DECONNEXION</a></li>';
+                    }?>!
 
             </ul>
         </div>
