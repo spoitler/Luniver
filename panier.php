1eb64a11357c98aafb3fa9ee26b57f0ca8f6f1f7
romain <romain.bonnes@gmail.com>
2019-12-05 17:16:41 +0100

    add bouton payment

--- panier.php
+++ panier.php
@@ -122,7 +122,7 @@ if (empty($ids)){
     </div>
 </div>
 <div>
-	<button>Confirmer la commande</button>
+	<a href="">Confirmer la commande</a>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 <script type="text/javascript" src="js/app.js"></script>
